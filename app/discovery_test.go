e1de12b05e026dc8d2f45e222715e893e8a2e0c2
bandesz <bandesz@gmail.com>
2018-12-14 11:26:21 +0000

    WIP: tests

--- app/discovery_test.go
+++ app/discovery_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	dto "github.com/prometheus/client_model/go"
+
 	"github.com/alphagov/paas-prometheus-exporter/app"
 	"github.com/alphagov/paas-prometheus-exporter/cf/mocks"
 	testmocks "github.com/alphagov/paas-prometheus-exporter/test/mocks"
@@ -14,6 +16,67 @@ import (
 	"github.com/cloudfoundry-community/go-cfclient"
 )
 
+var appFixture = cfclient.App{
+	Guid:      "33333333-3333-3333-3333-333333333333",
+	Instances: 1,
+	Name:      "foo",
+	State:     "STARTED",
+	SpaceData: cfclient.SpaceResource{
+		Entity: cfclient.Space{
+			Name: "spacename",
+			OrgData: cfclient.OrgResource{
+				Entity: cfclient.Org{Name: "orgname"},
+			},
+		},
+	},
+}
+
+func getMetrics(registerer *testmocks.FakeRegisterer) []prometheus.Metric {
+	metricsChan := make(chan prometheus.Metric, 1000)
+	for i := 0; i < registerer.RegisterCallCount(); i++ {
+		registerer.RegisterArgsForCall(i).Collect(metricsChan)
+	}
+	close(metricsChan)
+	metrics := make([]prometheus.Metric, 0)
+	for metric := range metricsChan {
+		metrics = append(metrics, metric)
+	}
+	return metrics
+}
+
+func metricHasLabels(metric prometheus.Metric, labels map[string]string) bool {
+	dtoMetric := &dto.Metric{}
+	metric.Write(dtoMetric)
+
+	actualLabels := make(map[string]string)
+	for _, pair := range dtoMetric.Label {
+		actualLabels[*pair.Name] = *pair.Value
+	}
+
+	for k, v := range labels {
+		if actualValue, ok := actualLabels[k]; !ok || actualValue != v {
+			return false
+		}
+	}
+
+	return true
+}
+
+func eventuallyRegistererWillHaveMetricWithLabels(
+	registerer *testmocks.FakeRegisterer,
+	labels map[string]string,
+) {
+	Eventually(func() bool {
+		for _, metric := range getMetrics(registerer) {
+			if metricHasLabels(metric, labels) {
+				return true
+			}
+		}
+
+		return false
+	}).Should(BeTrue(), "expected metric with labels: %s", labels)
+}
+
 var _ = Describe("CheckForNewApps", func() {
 
 	var discovery *app.Discovery
@@ -21,9 +84,12 @@ var _ = Describe("CheckForNewApps", func() {
 	var ctx context.Context
 	var cancel context.CancelFunc
 	var fakeRegisterer *testmocks.FakeRegisterer
+	var fakeAppStreamProvider *mocks.FakeAppStreamProvider
 
 	BeforeEach(func() {
 		fakeClient = &mocks.FakeClient{}
+		fakeAppStreamProvider = &mocks.FakeAppStreamProvider{}
+		fakeClient.NewAppStreamProviderReturns(fakeAppStreamProvider)
 		fakeRegisterer = &testmocks.FakeRegisterer{}
 		discovery = app.NewDiscovery(fakeClient, fakeRegisterer, 100*time.Millisecond)
 		ctx, cancel = context.WithCancel(context.Background())
@@ -39,13 +105,8 @@ var _ = Describe("CheckForNewApps", func() {
 		Eventually(fakeClient.ListAppsWithSpaceAndOrgCallCount).Should(Equal(2))
 	})
 
-	FIt("creates a new app", func() {
-		fakeClient.ListAppsWithSpaceAndOrgReturns([]cfclient.App{
-			{Guid: "33333333-3333-3333-3333-333333333333", Instances: 1, Name: "foo", State: "STARTED"},
-		}, nil)
-
-		fakeAppStreamProvider := &mocks.FakeAppStreamProvider{}
-		fakeClient.NewAppStreamProviderReturns(fakeAppStreamProvider)
+	It("creates a new app", func() {
+		fakeClient.ListAppsWithSpaceAndOrgReturns([]cfclient.App{appFixture}, nil)
 
 		go discovery.Start(ctx)
 
@@ -55,6 +116,10 @@ var _ = Describe("CheckForNewApps", func() {
 		Consistently(fakeAppStreamProvider.StartCallCount, 200*time.Millisecond).Should(Equal(1))
 
 		Eventually(fakeRegisterer.RegisterCallCount).Should(BeNumerically(">", 0))
+
+		eventuallyRegistererWillHaveMetricWithLabels(fakeRegisterer, map[string]string{
+			"guid": appFixture.Guid,
+		})
 	})
 
 	It("does not create a new appWatcher if the app state is stopped", func() {
@@ -62,21 +127,27 @@ var _ = Describe("CheckForNewApps", func() {
 			{Guid: "33333333-3333-3333-3333-333333333333", Instances: 1, Name: "foo", State: "STOPPED"},
 		}, nil)
 
-		e := app.NewDiscovery(fakeClient, prometheus.DefaultRegisterer, 100*time.Millisecond)
-
-		go e.Start(ctx)
+		go discovery.Start(ctx)
 
 		Consistently(fakeClient.NewAppStreamProviderCallCount, 200*time.Millisecond).Should(Equal(0))
+
+		Consistently(func() bool {
+			for _, metric := range getMetrics(fakeRegisterer) {
+				if metricHasLabels(metric, map[string]string{
+					"guid": "33333333-3333-3333-3333-333333333333",
+				}) {
+					return true
+				}
+			}
+			return false
+		}, 200*time.Millisecond).Should(BeFalse())
 	})
 
-	It("deletes an AppWatcher when an app is deleted", func() {
+	FIt("deletes an AppWatcher when an app is deleted", func() {
 		fakeClient.ListAppsWithSpaceAndOrgReturnsOnCall(0, []cfclient.App{
 			{Guid: "33333333-3333-3333-3333-333333333333", Instances: 1, Name: "foo", State: "STARTED"},
 		}, nil)
 		fakeClient.ListAppsWithSpaceAndOrgReturns([]cfclient.App{}, nil)
-
-		fakeAppStreamProvider := &mocks.FakeAppStreamProvider{}
-		fakeClient.NewAppStreamProviderReturns(fakeAppStreamProvider)
 
 		go discovery.Start(ctx)
 
@@ -86,6 +157,17 @@ var _ = Describe("CheckForNewApps", func() {
 
 		Consistently(fakeAppStreamProvider.StartCallCount, 200*time.Millisecond).Should(Equal(1))
 		Consistently(fakeAppStreamProvider.CloseCallCount, 200*time.Millisecond).Should(Equal(1))
+
+		Eventually(func() bool {
+			for _, metric := range getMetrics(fakeRegisterer) {
+				if metricHasLabels(metric, map[string]string{
+					"guid": "33333333-3333-3333-3333-333333333333",
+				}) {
+					return true
+				}
+			}
+			return false
+		}).Should(BeFalse())
 	})
 
 	It("deletes an AppWatcher when an app is stopped", func() {
@@ -95,9 +177,6 @@ var _ = Describe("CheckForNewApps", func() {
 		fakeClient.ListAppsWithSpaceAndOrgReturns([]cfclient.App{
 			{Guid: "11111111-11111-11111-1111-111-11-1-1-1", Instances: 1, Name: "foo", State: "STOPPED"},
 		}, nil)
-
-		fakeAppStreamProvider := &mocks.FakeAppStreamProvider{}
-		fakeClient.NewAppStreamProviderReturns(fakeAppStreamProvider)
 
 		go discovery.Start(ctx)
 
